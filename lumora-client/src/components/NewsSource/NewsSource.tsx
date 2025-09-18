4ae5f7ba2bdd3710ef334879f1596019f672e376
Itserge1 <realitserge@gmail.com>
2025-09-18 14:30:57 -0400

    add hero and news section

--- lumora-client/src/components/NewsSource/NewsSource.tsx
+++ lumora-client/src/components/NewsSource/NewsSource.tsx
@@ -2,10 +2,49 @@
 
 import styles from './NewsSource.module.css'
 
+import Image from "next/image";
+
 const NewsSource = () => {
     return(
         <section className={styles.newssource}>
-            <p>NewsSource Section</p>
+            <h4>NEWS PARTNERS</h4>
+            <div className={`${styles["newssource-icons-groups"]}`}>
+                <div className={`${styles["newssource-icon_yahoo_finance"]}`}>
+                    <Image
+                        src="/yahoo_finance.svg"
+                        alt="cnbc icons"
+                        width={150}
+                        height={150}
+                    />
+                </div>
+
+                <div className={`${styles["newssource-icon_cnbc"]}`}>
+                    <Image
+                        src="/icons8-nbc-color/icons8-nbc-480.png"
+                        alt="cnbc icons"
+                        width={50}
+                        height={50}
+                    /><span>CNBC</span>
+                </div>
+
+                <div>
+                    <Image
+                        src="/bloomberg.svg"
+                        alt="bloomberg icon"
+                        width={100}
+                        height={100}
+                    />
+                </div>
+
+                <div>
+                    <Image
+                        src="/The_Wall_Street_Journal.svg"
+                        alt="The Wall Street Journal icon"
+                        width={50}
+                        height={50}
+                    />
+                </div>
+            </div>
         </section>
     )
 }
