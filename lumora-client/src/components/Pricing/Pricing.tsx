6c937e6aebb001da8d41652f479f660ba750f151
Anna Mlodzianowska <90103679+amlodzianowska@users.noreply.github.com>
2025-09-25 13:03:01 -0500

    Add pricing section with purple theme colors (#15)
    * Pricing section-first draft
    
    * add section title
    
    * styling
    
    * add color schemes and, remove margin/padding breaking globals, style the pricing components
    
    * factor out pricing card
    
    * move interface up

--- lumora-client/src/components/Pricing/Pricing.tsx
+++ lumora-client/src/components/Pricing/Pricing.tsx
@@ -1,13 +1,79 @@
-"use client"
+"use client";
 
-import styles from './Pricing.module.css'
+import Button from "../Button/Button";
 
-const Pricing = () => {
-    return(
-        <section className={styles.pricing}>
-            <p>Pricing Section</p>
-        </section>
-    )
+interface PricingGroup {
+  title: string;
+  features: Array<{ label: string }>;
 }
 
-export default Pricing
+const PricingCard = ({ title, features }: PricingGroup) => {
+  return (
+    <div className="border rounded-3xl border-primary-500 shadow-md max-w-lg p-10 w-sm">
+      <h4 className="text-4xl text-center mt-4 font-extrabold">{title}</h4>
+      <ul className="my-10">
+        {features.map((feature) => (
+          <li
+            className="text-center text-lg mb-3 pb-3 border-b border-gray-700"
+            key={feature.label}
+          >
+            {feature.label}
+          </li>
+        ))}
+      </ul>
+      <Button />
+    </div>
+  );
+};
+
+const Pricing = () => {
+  const pricingGroups: PricingGroup[] = [
+    {
+      title: "Free",
+      features: [
+        { label: "5 watchlist stocks" },
+        { label: "Basic analytics" },
+        { label: "Email alerts" },
+        { label: "Daily news digest" },
+      ],
+    },
+    {
+      title: "Premium",
+      features: [
+        { label: "Unlimited watchlists" },
+        { label: "Fundamental analysis" },
+        { label: "Technical analysis" },
+        { label: "Retail & Suit Sentiment" },
+      ],
+    },
+    {
+      title: "Gold",
+      features: [
+        { label: "Unlimited watchlists" },
+        { label: "Fundamental analysis" },
+        { label: "Technical analysis" },
+        { label: "Retail & Suit Sentiment" },
+        { label: "Short interest" },
+      ],
+    },
+  ];
+
+  return (
+    <section className="bg-gray-800 text-white py-10 px-25">
+      <h2 className="text-center text-5xl font-extrabold tracking-tighter mb-10">
+        Choose Your Plan
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-3 justify-items-center max-w-10xl">
+        {pricingGroups.map((group) => (
+          <PricingCard
+            key={group.title}
+            title={group.title}
+            features={group.features}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Pricing;
