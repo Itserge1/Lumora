3b29ec2100b225d02e6bfd17b858212f903e09f4
Itserge1 <realitserge@gmail.com>
2025-09-26 15:01:09 -0400

    added watch list section

--- lumora-client/src/components/Hero/Hero.tsx
+++ lumora-client/src/components/Hero/Hero.tsx
@@ -9,8 +9,38 @@ import 'swiper/css/effect-coverflow';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
+interface SwiperSlideProps {
+    title: string,
+    description: string,
+}
 
 const Hero = () => {
+    const AllSwiperSlides: SwiperSlideProps[] = [
+        {
+            title: "Stock News Summaries",
+            description: "Get bite-sized summaries of the latest stock news in seconds"
+        },
+        {
+            title: "Market Movers Explained",
+            description: "Understand why your favorite stock is up 📈 or down 📉 today."
+        },
+        {
+            title: "Earnings Call Highlight",
+            description: "Cut through transcripts with concise takeaways that matter"
+        },
+        {
+            title: "Sector & Industry Trends",
+            description: "Spot which sectors are heating up or cooling off."
+        },
+        {
+            title: "Ticker-Based Feeds",
+            description: "Follow the news that actually matters for your watchlist"
+        },
+        {
+            title: "Sentiment Insights",
+            description: "See if the news is bullish 🐂 or bearish 🐻 at a glance."
+        }
+    ]
     return (
         <section className={styles.hero}>
             <div className={`${styles["swiper-container"]}`}>
@@ -35,57 +65,22 @@ const Hero = () => {
                         slideShadows: true,
                     }}
                     breakpoints={{
-                        // 560: {slidesPerView: 2.5},
                         768: {slidesPerView: 2},
                         1024: {slidesPerView: 1.5},
                         1280: {slidesPerView: 1.7},
                     }}
                     pagination={{clickable: true}}
-                    // modules={[EffectCoverflow, Pagination, Keyboard, Mousewheel]}
                     modules={[EffectCoverflow, Pagination, Keyboard, Autoplay]}
                     className="swiper"
                 >
-                    <SwiperSlide>
-                        <div className={`${styles['slide-content']}`}>
-                            <h3>Stock News Summaries</h3>
-                            <p>Get bite-sized summaries of the latest stock news in seconds</p>
-                        </div>
-                    </SwiperSlide>
-
-                    <SwiperSlide>
-                        <div className={`${styles['slide-content']}`}>
-                            <h3>Market Movers Explained</h3>
-                            <p>Understand why your favorite stock is up 📈 or down 📉 today.</p>
-                        </div>
-                    </SwiperSlide>
-
-                    <SwiperSlide>
-                        <div className={`${styles['slide-content']}`}>
-                            <h3>Earnings Call Highlights</h3>
-                            <p>Cut through transcripts with concise takeaways that matter</p>
-                        </div>
-                    </SwiperSlide>
-
-                    <SwiperSlide>
-                        <div className={`${styles['slide-content']}`}>
-                            <h3>Sector & Industry Trends</h3>
-                            <p>Spot which sectors are heating up—or cooling off.</p>
-                        </div>
-                    </SwiperSlide>
-
-                    <SwiperSlide>
-                        <div className={`${styles['slide-content']}`}>
-                            <h3>Ticker-Based Feeds</h3>
-                            <p>Follow the news that actually matters for your watchlist</p>
-                        </div>
-                    </SwiperSlide>
-
-                    <SwiperSlide>
-                        <div className={`${styles['slide-content']}`}>
-                            <h3>Sentiment Insights</h3>
-                            <p>See if the news is bullish 🐂 or bearish 🐻 at a glance.</p>
-                        </div>
-                    </SwiperSlide>
+                    {AllSwiperSlides.map((slide, i) => (
+                        <SwiperSlide key={i}>
+                            <div className={`${styles['slide-content']}`}>
+                                <h3>{slide.title}</h3>
+                                <p>{slide.description}</p>
+                            </div>
+                        </SwiperSlide>
+                    ))}
                 </Swiper>
             </div>
         </section>
