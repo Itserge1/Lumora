45fdbadfebd371ac83fc1515c91a822d160f3f80
Itserge1 <realitserge@gmail.com>
2025-10-13 13:12:43 -0400

    update gitignore, and add devops automation project

---
+++ Devops-Automation-Script/todo.py
@@ -0,0 +1,7 @@
+# Create a python cli tool that check if the name of the snapshot mane you pass actually exist on aws if not the it will
+# update the variable snapshot_identifier to ""
+
+# update the terraform-Iac-setup/terraform.tfvars file with the correct snapshot_identifier value before running terraform plan
+
+
+
